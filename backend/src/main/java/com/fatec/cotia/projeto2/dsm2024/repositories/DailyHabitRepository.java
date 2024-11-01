a69f4dd1132204086270bf4d1360337378f81f83
redlintles <treelands54@gmail.com>
2024-11-01 20:33:19 -0300

    Removido campo com chave estrangeira em DailyHabitRepo

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/repositories/DailyHabitRepository.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/repositories/DailyHabitRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fatec.cotia.projeto2.dsm2024.entities.DailyHabit;
 
 public interface DailyHabitRepository extends JpaRepository<DailyHabit, Long> {
-  public DailyHabit findByUsuario_CPF(Long Usuario_CPF);
+
 }

18e000163aa3fb3b07485b8da7cf0e1d645f9521
redlintles <treelands54@gmail.com>
2024-11-02 17:40:25 -0300

    Adicionado DTO FindCommonUser

---
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/dtos/commonUser/FindCommonUserDTO.java
@@ -0,0 +1,22 @@
+package com.fatec.cotia.projeto2.dsm2024.dtos.commonUser;
+
+public class FindCommonUserDTO {
+  private Long id;
+
+  public FindCommonUserDTO() {
+
+  }
+
+  public FindCommonUserDTO(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+}

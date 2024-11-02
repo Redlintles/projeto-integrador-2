7461a01006e6c8670a1b88b4ed447dd1ee4187c9
redlintles <treelands54@gmail.com>
2024-11-02 18:03:56 -0300

    Adicionado DTO CreateMedalDTO

---
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/dtos/medal/CreateMedalDTO.java
@@ -0,0 +1,65 @@
+package com.fatec.cotia.projeto2.dsm2024.dtos.medal;
+
+import com.fatec.cotia.projeto2.dsm2024.entities.CommonUser;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class CreateMedalDTO {
+  @NotNull
+  @Size(min = 45, max = 45)
+  private String nome;
+  @NotNull
+  @Size(min = 45, max = 45)
+  private String descricao;
+  @NotNull
+  @Size(min = 90, max = 90)
+  private String recompensa;
+  @NotNull
+  @Size(min = 11, max = 11)
+  private CommonUser usuario_CPF;
+
+  public CreateMedalDTO() {
+
+  }
+
+  public CreateMedalDTO(String nome, String descricao, String recompensa, CommonUser usuario_CPF) {
+    this.nome = nome;
+    this.descricao = descricao;
+    this.recompensa = recompensa;
+    this.usuario_CPF = usuario_CPF;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public String getRecompensa() {
+    return recompensa;
+  }
+
+  public void setRecompensa(String recompensa) {
+    this.recompensa = recompensa;
+  }
+
+  public CommonUser getUsuario_CPF() {
+    return usuario_CPF;
+  }
+
+  public void setUsuario_CPF(CommonUser usuario_CPF) {
+    this.usuario_CPF = usuario_CPF;
+  }
+
+}

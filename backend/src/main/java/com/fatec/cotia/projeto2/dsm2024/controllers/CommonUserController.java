be4a78990f224dfe03e0b2d6423ce3fa610b0d0f
redlintles <treelands54@gmail.com>
2024-11-07 20:15:14 -0300

    Adicionado CreateUser a CommonUserController

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/CommonUserController.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/CommonUserController.java
@@ -3,12 +3,16 @@ package com.fatec.cotia.projeto2.dsm2024.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatec.cotia.projeto2.dsm2024.dtos.commonUser.CreateCommonUserDTO;
 import com.fatec.cotia.projeto2.dsm2024.dtos.commonUser.FindCommonUserDTO;
 import com.fatec.cotia.projeto2.dsm2024.entities.CommonUser;
 import com.fatec.cotia.projeto2.dsm2024.services.CommonUserService;
@@ -30,6 +34,17 @@ public class CommonUserController {
       return ResponseEntity.ok(user.get());
     } else {
       return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PostMapping("/")
+  public ResponseEntity<CommonUser> createUser(@RequestBody CreateCommonUserDTO data) {
+    Optional<CommonUser> newUser = this.commonUserService.createUser(data);
+
+    if (newUser.isPresent()) {
+      return ResponseEntity.status(HttpStatus.CREATED).body(newUser.get());
+    } else {
+      return ResponseEntity.badRequest().build();
     }
   }
 }

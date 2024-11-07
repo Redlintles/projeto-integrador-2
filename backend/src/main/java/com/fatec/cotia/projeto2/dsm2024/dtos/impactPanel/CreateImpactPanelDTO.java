b8bd1dbd8bfaf235435c48d2e39523a7d49e2990
redlintles <treelands54@gmail.com>
2024-11-07 20:47:06 -0300

    Corrigido erro em ImpactPanelDTO

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/dtos/impactPanel/CreateImpactPanelDTO.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/dtos/impactPanel/CreateImpactPanelDTO.java
@@ -1,14 +1,12 @@
 package com.fatec.cotia.projeto2.dsm2024.dtos.impactPanel;
 
-import com.fatec.cotia.projeto2.dsm2024.entities.CommonUser;
-
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class CreateImpactPanelDTO {
   @NotNull
   @Size(min = 11, max = 11)
-  private CommonUser Usuario_cpf;
+  private String Usuario_cpf;
   @Size(min = 0, max = 90)
   @NotNull
   private String impactoIndividual;
@@ -20,17 +18,17 @@ public class CreateImpactPanelDTO {
 
   }
 
-  public CreateImpactPanelDTO(CommonUser Usuario_cpf, String impactoIndividual, String impactColetivo) {
+  public CreateImpactPanelDTO(String Usuario_cpf, String impactoIndividual, String impactColetivo) {
     this.Usuario_cpf = Usuario_cpf;
     this.impactoColetivo = impactColetivo;
     this.impactoIndividual = impactoIndividual;
   }
 
-  public CommonUser getUsuario_cpf() {
+  public String getUsuario_cpf() {
     return Usuario_cpf;
   }
 
-  public void setUsuario_cpf(CommonUser Usuario_cpf) {
+  public void setUsuario_cpf(String Usuario_cpf) {
     this.Usuario_cpf = Usuario_cpf;
   }
 

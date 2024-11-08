1993d7b9fe527491fb836e90346929dba1b1e58d
redlintles <treelands54@gmail.com>
2024-11-08 20:39:56 -0300

    Métodos do repositório CommonUserRepository Alterados para retornar Optional

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/repositories/CommonUserRepository.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/repositories/CommonUserRepository.java
@@ -1,13 +1,15 @@
 package com.fatec.cotia.projeto2.dsm2024.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fatec.cotia.projeto2.dsm2024.entities.CommonUser;
 
 public interface CommonUserRepository extends JpaRepository<CommonUser, Long> {
-  public CommonUser findByCpf(String cpf);
+  public Optional<CommonUser> findByCpf(String cpf);
 
-  public CommonUser findByNome(String nome);
+  public Optional<CommonUser> findByNome(String nome);
 
-  public CommonUser findByEmail(String email);
+  public Optional<CommonUser> findByEmail(String email);
 }

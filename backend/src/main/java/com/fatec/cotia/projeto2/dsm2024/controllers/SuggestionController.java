20c48978db41a65d55320805cf3878ee2134352e
redlintles <treelands54@gmail.com>
2024-11-08 18:34:00 -0300

    Adicionado rota CreateSuggestion em SuggestionController

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/SuggestionController.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/SuggestionController.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatec.cotia.projeto2.dsm2024.dtos.suggestion.CreateSuggestionDTO;
 import com.fatec.cotia.projeto2.dsm2024.dtos.suggestion.FindSuggestionDTO;
 import com.fatec.cotia.projeto2.dsm2024.entities.Suggestion;
 import com.fatec.cotia.projeto2.dsm2024.services.SuggestionService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/suggestion")
@@ -29,4 +34,16 @@ public class SuggestionController {
       return ResponseEntity.notFound().build();
     }
   }
+
+  @PostMapping
+  public ResponseEntity<Suggestion> createSuggestion(@Valid @RequestBody CreateSuggestionDTO data) {
+    Optional<Suggestion> newSuggestion = this.suggestionService.createSuggestion(data);
+
+    if (newSuggestion.isPresent()) {
+      return ResponseEntity.ok(newSuggestion.get());
+    } else {
+      return ResponseEntity.badRequest().build();
+    }
+  }
+
 }

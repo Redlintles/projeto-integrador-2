42dfb582da4de779e2eb275278d4009499324883
redlintles <treelands54@gmail.com>
2024-11-08 15:34:35 -0300

    Adicionado Método Delete à SuggestionService

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/services/SuggestionService.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/services/SuggestionService.java
@@ -1,11 +1,14 @@
 package com.fatec.cotia.projeto2.dsm2024.services;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.fatec.cotia.projeto2.dsm2024.dtos.suggestion.CreateSuggestionDTO;
+import com.fatec.cotia.projeto2.dsm2024.dtos.suggestion.DeleteSuggestionDTO;
 import com.fatec.cotia.projeto2.dsm2024.dtos.suggestion.FindSuggestionDTO;
+import com.fatec.cotia.projeto2.dsm2024.dtos.suggestion.UpdateSuggestionDTO;
 import com.fatec.cotia.projeto2.dsm2024.entities.Suggestion;
 import com.fatec.cotia.projeto2.dsm2024.repositories.SuggestionRepository;
 
@@ -28,4 +31,43 @@ public class SuggestionService {
     return this.suggestionRepository.findById(data.getId());
   }
 
+  public Optional<HashMap<String, Suggestion>> updateSuggestion(Long id, UpdateSuggestionDTO data) {
+
+    Optional<Suggestion> toUpdate = this.suggestionRepository.findById(id);
+
+    if (toUpdate.isEmpty()) {
+      return null;
+    }
+
+    HashMap<String, Suggestion> returnValue = new HashMap<>();
+    returnValue.put("Old", toUpdate.get());
+
+    Suggestion copy = new Suggestion(toUpdate.get());
+
+    if (data.getReducaoCO2Estimado() != null) {
+      copy.setReducaoCO2Estimado(data.getReducaoCO2Estimado());
+    }
+
+    if (data.getUsuario_CPF() != null) {
+      copy.setUsuario_CPF(data.getUsuario_CPF());
+    }
+
+    Suggestion updatedSuggestion = this.suggestionRepository.save(copy);
+
+    returnValue.put("New", updatedSuggestion);
+
+    return Optional.of(returnValue);
+
+  }
+
+  public Optional<Suggestion> deleteSuggestionById(DeleteSuggestionDTO data) {
+    Optional<Suggestion> toDelete = this.suggestionRepository.findById(data.getId());
+
+    if (toDelete.isEmpty()) {
+      return null;
+    } else {
+      this.suggestionRepository.deleteById(data.getId());
+      return toDelete;
+    }
+  }
 }

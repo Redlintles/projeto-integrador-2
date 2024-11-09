ba1618bda7fd596d42db9bae120f88b20d838075
redlintles <treelands54@gmail.com>
2024-11-09 18:07:45 -0300

    Adicionado Controlador ImpactPanelController

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/ImpactPanelController.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/ImpactPanelController.java
@@ -1,5 +1,43 @@
 package com.fatec.cotia.projeto2.dsm2024.controllers;
 
-public class ImpactPanelController {
+import java.util.HashMap;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fatec.cotia.projeto2.dsm2024.dtos.ImpactPanelDTO;
+import com.fatec.cotia.projeto2.dsm2024.entities.ImpactPanel;
+import com.fatec.cotia.projeto2.dsm2024.services.ImpactPanelService;
+
+public class ImpactPanelController {
+  @Autowired
+  private ImpactPanelService impactPanelService;
+
+  @GetMapping("/{id}")
+  public ResponseEntity<ImpactPanel> findImpactPanelById(@PathVariable Long id) {
+    Optional<ImpactPanel> register = this.impactPanelService.findById(id);
+
+    if (register.isPresent()) {
+      return ResponseEntity.ok(register.get());
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PatchMapping("/{id}")
+  public ResponseEntity<HashMap<String, ImpactPanel>> updateImpactPanelById(@PathVariable Long id,
+      ImpactPanelDTO data) {
+
+    Optional<HashMap<String, ImpactPanel>> result = this.impactPanelService.updateImpactPanel(id, data);
+    if (result.isPresent()) {
+      return ResponseEntity.ok(result.get());
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+
+  }
 }

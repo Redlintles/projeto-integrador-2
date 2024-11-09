ac515107cb33dc9dadc4951fa12ab53c16e18a61
redlintles <treelands54@gmail.com>
2024-11-09 18:30:28 -0300

    Adicionado e finalizado MedalController

---
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/MedalController.java
@@ -0,0 +1,68 @@
+package com.fatec.cotia.projeto2.dsm2024.controllers;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.fatec.cotia.projeto2.dsm2024.dtos.MedalDTO;
+import com.fatec.cotia.projeto2.dsm2024.entities.Medal;
+import com.fatec.cotia.projeto2.dsm2024.interfaces.CreationGroupInterface;
+import com.fatec.cotia.projeto2.dsm2024.interfaces.UpdateGroupInterface;
+import com.fatec.cotia.projeto2.dsm2024.services.MedalService;
+
+public class MedalController {
+  @Autowired
+  private MedalService medalService;
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Medal> findMedalById(@PathVariable Long id) {
+    Optional<Medal> result = this.medalService.findById(id);
+
+    if (result.isPresent()) {
+      return ResponseEntity.ok(result.get());
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PostMapping
+  public ResponseEntity<Medal> createMedal(@Validated(CreationGroupInterface.class) @RequestBody MedalDTO data) {
+    Optional<Medal> result = this.medalService.createMedal(data);
+    if (result.isPresent()) {
+      return ResponseEntity.ok(result.get());
+    } else {
+      return ResponseEntity.badRequest().build();
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Medal> deleteMedalById(@PathVariable Long id) {
+    Optional<Medal> result = this.medalService.deleteById(id);
+
+    if (result.isPresent()) {
+      return ResponseEntity.ok(result.get());
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PatchMapping("/{id}")
+  public ResponseEntity<HashMap<String, Medal>> updateMedal(@PathVariable Long id,
+      @Validated(UpdateGroupInterface.class) @RequestBody MedalDTO data) {
+    Optional<HashMap<String, Medal>> result = this.medalService.updateMedal(id, data);
+    if (result.isPresent()) {
+      return ResponseEntity.ok(result.get());
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+}

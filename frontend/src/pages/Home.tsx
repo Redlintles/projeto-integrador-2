2602ebc05a0074e52501f9bbf9d7fa2aff686a65
redlintles <treelands54@gmail.com>
2024-11-14 17:54:59 -0300

    Finalizada marcação de Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,22 @@
-import React from "react";
-
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="home">
+      <div className="home-logo">
+        <img src="" alt="" className="home-logo__img" />
+        <h1 className="home-logo__title">Green Action</h1>
+        <p className="home-logo__text">Engajando ações</p>
+      </div>
+      <div className="home-actions">
+        <p className="home-actions__text">
+          Reduza sua pegada de carbono e faça parte da mudança para um futuro
+          sustentável
+        </p>
+        <button className="home-action__btn register">Comece Agora</button>
+        <button className="home-action__btn login">Já tenho uma conta</button>
+      </div>
+      <p className="home__text">
+        Receba sugestões personalizadas de acordo com o seu estilo de vida!
+      </p>
+    </section>
   );
 }

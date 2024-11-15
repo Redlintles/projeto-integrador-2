efd391f13913a5371d7eab4473c6867e4706ce6f
redlintles <treelands54@gmail.com>
2024-11-15 19:46:58 -0300

    Adicionados estilos de Home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,20 +1,31 @@
+import styles from "./Home.module.css";
 export default function Home() {
   return (
-    <section className="home">
-      <div className="home-logo">
-        <img src="" alt="" className="home-logo__img" />
-        <h1 className="home-logo__title">Green Action</h1>
-        <p className="home-logo__text">Engajando ações</p>
+    <section className={styles["home"]}>
+      <div className={styles["home-logo"]}>
+        <img src="" alt="" className={styles["home-logo__img"]} />
+        <h1 className={styles["home-logo__title"]}>Green Action</h1>
+        <p className={styles["home-logo__text"]}>
+          Engajando ações, conscientizando o futuro
+        </p>
       </div>
-      <div className="home-actions">
-        <p className="home-actions__text">
+      <div className={styles["home-actions"]}>
+        <p className={styles["home-actions__text"]}>
           Reduza sua pegada de carbono e faça parte da mudança para um futuro
           sustentável
         </p>
-        <button className="home-action__btn register">Comece Agora</button>
-        <button className="home-action__btn login">Já tenho uma conta</button>
+        <button
+          className={`${styles["home-actions__btn"]} ${styles["home-actions__btn--register"]}`}
+        >
+          Comece Agora
+        </button>
+        <button
+          className={`${styles["home-actions__btn"]} ${styles["home-actions__btn--login"]}`}
+        >
+          Já tenho uma conta
+        </button>
       </div>
-      <p className="home__text">
+      <p className={styles["home__text"]}>
         Receba sugestões personalizadas de acordo com o seu estilo de vida!
       </p>
     </section>

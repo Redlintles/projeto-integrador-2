885042451da6bb0112903a33ad14767925748cf2
redlintles <treelands54@gmail.com>
2024-11-16 21:48:37 -0300

    Iniciado o desenvolvimento de Initial.tsx

--- frontend/src/pages/Initial/Initial.tsx
+++ frontend/src/pages/Initial/Initial.tsx
@@ -1,11 +1,68 @@
+import { Link } from "react-router-dom";
+import AsideLoggedIn from "../../components/AsideLoggedIn/AsideLoggedIn";
 import styles from "./Initial.module.css";
-import AsideLoggedIn from "../../../.history/src/components/AsideLoggedIn_20241114194110";
+import home1 from "../../assets/home-1.png";
+import home2 from "../../assets/home-2.png";
+import home3 from "../../assets/home-3.png";
 
 export default function Initial() {
   return (
     <section className={styles["initial"]}>
       <AsideLoggedIn fullName="João Paulo" />
-      <h1>Hello WOrld!</h1>
+      <div className={styles["initial-main"]}>
+        <div
+          className={`${styles["initial-main__info"]} ${styles["initial-main__info--suggestion"]}`}
+        >
+          <Link
+            to="/suggestion"
+            className={styles["initial-main__link"]}
+          ></Link>
+          <h4 className={styles["initial-main__title"]}>
+            Sugestões de Ações Diárias
+          </h4>
+          <div>
+            <div className={styles["initial-main__text"]}>
+              <p>
+                Diariamente acompanhe as sugestões de novos hábitos a serem
+                inseridos em sua rotina!
+              </p>
+              <p>
+                As sugestões visam vantagens para o meio ambiente e você mesmo,
+                a cada ação feita, você receberá 10 pontos que irão de ajudar a
+                crescer no ranking semanal e a conquistar medalhas
+              </p>
+            </div>
+            <img src={home1} alt="" className={styles["initial-main__img"]} />
+          </div>
+        </div>
+        <div
+          className={`${styles["initial-main__info"]} ${styles["initial-main__info--ranking"]}`}
+        >
+          <Link to="/ranking" className={styles["initial-main__link"]}></Link>
+          <h4>Ranking Semanal</h4>
+          <div className={styles["initial-main__text"]}>
+            <p>
+              Semanalmente acompanhe o seu progresso e o compare com o de outros
+              usuários, garantindo pontos extras se ficar em primeiro lugar
+            </p>
+          </div>
+          <img src={home2} alt="" className={styles["initial-main__img"]} />
+        </div>
+        <div
+          className={`${styles["initial-main__info"]} ${styles["initial-main__info--medals"]}`}
+        >
+          <Link to="/medals" className={styles["initial-main__link"]}></Link>
+          <h4>Medalhas</h4>
+          <div className={styles["initial-main__text"]}>
+            <p>
+              Ao arrecadar pontos e avançar no ranking semanal, garanta medalhas
+              exclusivas.
+            </p>
+            <p>Cada medalha ganha, vem acompanhada de uma recompensa</p>
+          </div>
+          <img src={home3} alt="" className={styles["initial-main__img"]} />
+        </div>
+      </div>
     </section>
   );
 }

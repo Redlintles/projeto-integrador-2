84cf2c35920cc8a5aabebfc7117677279979d021
redlintles <treelands54@gmail.com>
2024-11-19 17:46:33 -0300

    Enable Scheduling adicionado

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/ProjetoIntegrador2SemestreApplication.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/ProjetoIntegrador2SemestreApplication.java
@@ -2,7 +2,9 @@ package com.fatec.cotia.projeto2.dsm2024;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ProjetoIntegrador2SemestreApplication {
 

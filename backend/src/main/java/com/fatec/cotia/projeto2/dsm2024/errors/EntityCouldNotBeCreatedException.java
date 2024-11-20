f162ebf71710376723555b3a2bbbc88d6e7e9369
redlintles <treelands54@gmail.com>
2024-11-20 20:30:26 -0300

    Adicionada Exceção EntiyCouldNotBeCreatedException

---
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/errors/EntityCouldNotBeCreatedException.java
@@ -0,0 +1,7 @@
+package com.fatec.cotia.projeto2.dsm2024.errors;
+
+public class EntityCouldNotBeCreatedException extends RuntimeException {
+  public EntityCouldNotBeCreatedException(String message) {
+    super(message);
+  }
+}

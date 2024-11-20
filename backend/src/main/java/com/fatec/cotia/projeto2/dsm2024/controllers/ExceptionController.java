fa0ef0f03bbd7daf30533b52e0715c2f0423e90f
redlintles <treelands54@gmail.com>
2024-11-20 19:34:05 -0300

    Adicionado tratamento de Exceção de TokenAlreadyExists

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/ExceptionController.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/ExceptionController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.fatec.cotia.projeto2.dsm2024.errors.TokenAlreadyExistsException;
 import com.fatec.cotia.projeto2.dsm2024.errors.UserNotFoundException;
 import com.fatec.cotia.projeto2.dsm2024.responses.ErrorResponse;
 
@@ -15,6 +16,13 @@ public class ExceptionController {
   public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex) {
     ErrorResponse response = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
     return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(TokenAlreadyExistsException.class)
+  public ResponseEntity<ErrorResponse> handleTokenAlreadyExists(TokenAlreadyExistsException ex) {
+    ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
 
 }

8401e4190e5d4b87ab18ae42a4b89f6dd649d44a
redlintles <treelands54@gmail.com>
2024-11-20 19:26:24 -0300

    Modificado herança de UserNotFoundException de Exception para RuntimeException

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/errors/UserNotFoundException.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/errors/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.fatec.cotia.projeto2.dsm2024.errors;
 
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends RuntimeException {
   public UserNotFoundException(String message) {
     super(message);
   }

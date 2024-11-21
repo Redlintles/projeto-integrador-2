17c540a9b5728c5d8bcadd7efeb0d99d2e1faa76
redlintles <treelands54@gmail.com>
2024-11-21 17:30:53 -0300

    Adicionada exceção EntityCouldNotBeUpdated

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/ExceptionController.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/ExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.fatec.cotia.projeto2.dsm2024.errors.EntityCouldNotBeCreatedException;
+import com.fatec.cotia.projeto2.dsm2024.errors.EntityCouldNotBeUpdatedException;
 import com.fatec.cotia.projeto2.dsm2024.errors.EntityNotFoundException;
 import com.fatec.cotia.projeto2.dsm2024.errors.InvalidInputDataException;
 import com.fatec.cotia.projeto2.dsm2024.errors.TokenAlreadyExistsException;
@@ -39,6 +40,13 @@ public class ExceptionController {
     ErrorResponse response = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.value(), null);
 
     return new ResponseEntity<>(response, HttpStatus.UNPROCESSABLE_ENTITY);
+  }
+
+  @ExceptionHandler(EntityCouldNotBeUpdatedException.class)
+  public ResponseEntity<ErrorResponse> handleEntityCouldNotBeUpdated(EntityCouldNotBeUpdatedException ex) {
+    ErrorResponse response = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+
+    return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
 }
---
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/errors/EntityCouldNotBeUpdatedException.java
@@ -0,0 +1,7 @@
+package com.fatec.cotia.projeto2.dsm2024.errors;
+
+public class EntityCouldNotBeUpdatedException extends RuntimeException {
+  public EntityCouldNotBeUpdatedException(String message) {
+    super(message);
+  }
+}

1284b9e58f800a3b3dbc05d4f53596d9ef22de63
redlintles <treelands54@gmail.com>
2024-11-22 20:39:02 -0300

    Adicionados métodos findByToken e existsByToken em TokenRepository

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/repositories/TokenRepository.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/repositories/TokenRepository.java
@@ -13,4 +13,8 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
   public void deleteByCreatedAtBefore(LocalDateTime dateTIme);
 
   public Optional<Token> findByCpf(String cpf);
+
+  public Optional<Token> findByToken(String Token);
+
+  public boolean existsByToken(String token);
 }

b414c7f924aa5b685f0f3e8ed6d9b0d1930cf028
redlintles <treelands54@gmail.com>
2024-11-22 21:03:00 -0300

    Modificado Hello para utilizar StandardResponse

--- backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/Hello.java
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/Hello.java
@@ -1,13 +1,18 @@
 package com.fatec.cotia.projeto2.dsm2024.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fatec.cotia.projeto2.dsm2024.responses.StandardResponse;
 
 @RestController()
 public class Hello {
   @GetMapping("hello")
-  public String hello() {
+  public ResponseEntity<StandardResponse<String>> hello() {
 
-    return "Hello World! Jigoku no shinpan";
+    StandardResponse<String> response = new StandardResponse<>("Hello World!", "Server is up and running!");
+
+    return ResponseEntity.ok(response);
   }
 }

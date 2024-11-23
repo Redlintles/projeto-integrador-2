2faa67b4695ffe11288dc2ab83c7ddd239a6d091
redlintles <treelands54@gmail.com>
2024-11-23 18:46:01 -0300

    Adicionado controlador TokenController

---
+++ backend/src/main/java/com/fatec/cotia/projeto2/dsm2024/controllers/TokenController.java
@@ -0,0 +1,28 @@
+package com.fatec.cotia.projeto2.dsm2024.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fatec.cotia.projeto2.dsm2024.responses.StandardResponse;
+import com.fatec.cotia.projeto2.dsm2024.services.TokenService;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+  @Autowired
+  private TokenService tokenService;
+
+  @GetMapping("/valid/{token}")
+  public ResponseEntity<StandardResponse<Boolean>> isValid(@PathVariable String token) {
+
+    Boolean isValid = this.tokenService.isValidToken(token);
+    StandardResponse<Boolean> response = new StandardResponse<>(isValid, "Token verificado com sucesso");
+
+    return ResponseEntity.ok(response);
+
+  }
+}
